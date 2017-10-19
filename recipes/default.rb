345c4606f655e445d740cbba23dd2df7cd586114
vasilios hantzis <vchantzis@yahoo.com>
2017-10-19 11:43:20 -0400

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: update
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+apt_update 'update' do
+       action :update
+end
+
